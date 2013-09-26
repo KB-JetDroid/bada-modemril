63d093cacee07b00ad023d5666b000d9c5911e57
Nikolay <volk204@mail.ru>
2013-09-26 23:52:12 +0400

    [CALL] Fixed HANGUP_WAITING_OR_BACKGROUND
    Change-Id: I6b1318a8e8b1bc3ecdfdc916afa2e906f257556f

--- libmocha-ipc/mocha-ril/call.c
+++ libmocha-ipc/mocha-ril/call.c
@@ -446,21 +446,63 @@ error:
 void ril_request_hangup_waiting_or_background(RIL_Token t)
 {
 	int i;
-	ALOGE("%s: Test me!", __func__);
-	
+	uint32_t activeId = 0xFFFFFFFF;
+	uint32_t holdId = 0xFFFFFFFF;
+	uint32_t waitId = 0xFFFFFFFF;
+	uint32_t callType = 0xFFFFFFFF;
 	for(i = 0; i < MAX_CALLS; i++)
 	{
-		if(ril_data.calls[i] && ril_data.calls[i]->callId != 0xFFFFFFFF && 
-			(ril_data.calls[i]->call_state == RIL_CALL_INCOMING || ril_data.calls[i]->call_state == RIL_CALL_WAITING || ril_data.calls[i]->call_state == RIL_CALL_HOLDING))
+		if(ril_data.calls[i] && ril_data.calls[i]->callId != 0xFFFFFFFF)
 		{
-			ALOGE("%s: hanging up callId = %d", __func__, ril_data.calls[i]->callId);
-			if (ril_data.calls[i]->token != 0)
-				/* pass error to the current request, another one is pending */
-				goto error;
-			ril_data.calls[i]->token = t;
-			tapi_call_release(ril_data.calls[i]->callType, ril_data.calls[i]->callId, 0x0);
-			break;
+			if(ril_data.calls[i]->call_state == RIL_CALL_ACTIVE)
+			{
+				activeId = ril_data.calls[i]->callId;
+				if (ril_data.calls[i]->token != 0)
+					/* pass error to the current request, another one is pending */
+					goto error;
+				ril_data.calls[i]->token = t;
+				ALOGE("%s: active callId = %d", __func__, activeId);
+			}
+			if(ril_data.calls[i]->call_state == RIL_CALL_HOLDING)
+			{
+				holdId = ril_data.calls[i]->callId;
+				if (ril_data.calls[i]->token != 0)
+					/* pass error to the current request, another one is pending */
+					goto error;
+				ril_data.calls[i]->token = t;
+				ALOGE("%s: hold callId = %d", __func__, holdId);
+			}
+			if(ril_data.calls[i]->call_state == RIL_CALL_WAITING)
+			{
+				waitId = ril_data.calls[i]->callId;
+				callType = ril_data.calls[i]->callType;
+				if (ril_data.calls[i]->token != 0)
+					/* pass error to the current request, another one is pending */
+					goto error;
+				ril_data.calls[i]->token = t;
+				ALOGE("%s: wait callId = %d", __func__, waitId);
+			}
 		}
+	}
+	if(waitId != 0xFFFFFFFF && holdId != 0xFFFFFFFF)
+	{
+		ALOGE("%s: waiting/hangup callId = %d", __func__, waitId);
+		tapi_call_release(callType, waitId, 0x0);
+	}
+	else if(activeId != 0xFFFFFFFF)
+	{
+		ALOGE("%s: active/hangup callId = %d", __func__, activeId);
+		tapi_call_release(callType, activeId, 0x0);
+	}
+	else if(holdId != 0xFFFFFFFF)
+	{
+		ALOGE("%s: hold/hangup callId = %d", __func__, holdId);
+		tapi_call_release(callType, holdId, 0x0);
+	}
+	else if(waitId != 0xFFFFFFFF)
+	{
+		ALOGE("%s: wait/hangup callId = %d", __func__, waitId);
+		tapi_call_release(callType, waitId, 0x0);
 	}
 	return;
 error:
